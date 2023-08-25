1e7532ee08eb627bd26775e13b64c2fc40feaffe
MiloshBogdanovic <47362505+MiloshBogdanovic@users.noreply.github.com>
2023-08-25 17:47:26 +0200

    Freash

--- myportfolio/cv/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from .views import UserCVListCreateView
-from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
-
-urlpatterns = [
-    path('cv/', UserCVListCreateView.as_view(), name='cv-detail'),
-    path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
-    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
-]
